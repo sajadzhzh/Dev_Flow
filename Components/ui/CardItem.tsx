87ad8359dc9df76df9740725bfadfaef8e496e7f
unknown <zhiyanjoo1382@gmail.com>
2026-06-15 16:49:05 +0330

    Added priority to cards

--- Components/ui/CardItem.tsx
+++ Components/ui/CardItem.tsx
@@ -1,23 +1,62 @@
 import { Box } from "lucide-react";
 
-export default function CardItem() {
+export default function CardItem({
+  priority,
+  name,
+  lastUpdate,
+  percent
+}: {
+  priority: "critical" | "high" | "mid" | "low";
+  name: string;
+  lastUpdate: string;
+  percent: number;
+}) {
   return (
     <>
       <div className="flex items-center justify-between gap-2 my-2 flex-wrap">
         <div className="flex gap-2">
-          <div className="p-3 bg-[#8a5cf65b] text-[#8B5CF6] rounded-2xl">
-            <Box />
-          </div>
+          {priority === "critical" && (
+            <div className="p-3 bg-[#8a5cf65b] text-[#8B5CF6] rounded-2xl">
+              <Box />
+            </div>
+          )}
+          {priority === "high" && (
+            <div className="p-3 bg-[#3b83f662] text-[#3B82F6] rounded-2xl">
+              <Box />
+            </div>
+          )}
+          {priority === "mid" && (
+            <div className="p-3 bg-[#f59f0b62] text-[#F59E0B] rounded-2xl">
+              <Box />
+            </div>
+          )}
+          {priority === "low" && (
+            <div className="p-3 bg-[#22c55e62] text-[#22C55E] rounded-2xl">
+              <Box />
+            </div>
+          )}
+
           <div className="flex-col gap-2 text-sm lg:text-md">
-            طراحی وب‌سایت فروشگاهی
+            {name}
             <div className="text-gray-500 text-sm">
-              آخرین بروزرسانی : 2 ساعت پیش
+              آخرین بروزرسانی : {lastUpdate} ساعت پیش
             </div>
           </div>
         </div>
         <div className="grow min-w-1/2 h-max border-b-2 border-gray-700 rounded relative mt-2">
-          <p className="absolute left-0 -top-5">50%</p>
-          <div className="w-[50%] absolute left-0 top-0 border-t-2 border-[#8B5CF6] h-max rounded"></div>
+          <p className="absolute left-0 -top-5">{percent}%</p>
+          {priority === "critical" && (
+            <div className="w-[50%] absolute left-0 top-0 border-t-2 border-[#8B5CF6] h-max rounded"></div>
+          )}
+          {priority === "high" && (
+            <div className="w-[50%] absolute left-0 top-0 border-t-2 border-[#3B82F6] h-max rounded"></div>
+          )}
+          {priority === "mid" && (
+            <div className="w-[50%] absolute left-0 top-0 border-t-2 border-[#F59E0B] h-max rounded"></div>
+          )}
+          {priority === "low" && (
+            <div className="w-[50%] absolute left-0 top-0 border-t-2 border-[#22C55E] h-max rounded"></div>
+          )}
         </div>
       </div>
     </>
--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -3,6 +3,7 @@ import CardItem from "@/Components/ui/CardItem";
 import Card from "@/Components/ui/SmallCard";
 import { LayoutGrid } from "lucide-react";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 export default function DashboardPage() {
   const { data: session } = useSession();
@@ -30,12 +31,39 @@ export default function DashboardPage() {
 
       <div className="w-full flex gap-5 flex-wrap flex-col lg:flex-row">
         <div className="grow lg:max-w-1/2 border border-gray-600 bg-[#18181B] rounded-xl py-3 px-5"></div>
-        <div className="grow lg:max-w-1/2 border border-gray-600 bg-[#18181B] rounded-xl py-3 px-5">
+        <div className="grow lg:max-w-1/2 border border-gray-600 bg-[#18181B] rounded-xl py-3 px-5 h-max">
           <div className="w-full text-sm text-gray-400 mb-5">
             پیشرفت پروژه‌ها
           </div>
 
-          <CardItem />
+          <CardItem
+            priority="critical"
+            name="طراحی سایت فروشگاهی"
+            lastUpdate="2"
+            percent={50}
+          />
+          <CardItem
+            priority="high"
+            name="طراحی سایت فروشگاهی"
+            lastUpdate="2"
+            percent={50}
+          />
+          <CardItem
+            priority="mid"
+            name="طراحی سایت فروشگاهی"
+            lastUpdate="2"
+            percent={50}
+          />
+          <CardItem
+            priority="low"
+            name="طراحی سایت فروشگاهی"
+            lastUpdate="2"
+            percent={50}
+          />
+
+          <div className="w-full text-sm flex justify-end text-gray-400 mt-5">
+            <Link href="" className="p-1 px-2 rounded-xl bg-white/25 hover:bg-white/50 hover:text-gray-900">مشاهده همه پروژه‌ها</Link>
+          </div>
         </div>
       </div>
     </div>
